a4d4559003d2f200c15ca81410a2e0e54958a08f
Pedro Santos <pedrohrsantos01@gmail.com>
2022-01-03 10:18:32 -0300

    Criando Tema global

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+    colors: {
+        primary: '#5636D3',
+
+        secondary: '#FF872C',
+        secondary_light: 'rgba(255, 135, 44, 0.3)',
+
+        sucess: '#12A454',
+        sucess_light: 'rgba(18, 164, 84, 0.5)',
+
+        attention: '#e83f5b',
+        attention_light: 'rgba(232, 63, 91, 0.5)',
+
+        shape: '#ffff',
+        title: '#363f5f',
+        text: '#969cb2',
+        background: '#f0f2f5',
+    }
+} 
