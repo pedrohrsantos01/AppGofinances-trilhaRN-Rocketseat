f441010fe460b34f6fbb52e56fc31237a1701550
Pedro Santos <pedrohrsantos01@gmail.com>
2022-03-17 18:22:23 -0300

    Criação do button para enviar as transações

--- App.tsx
+++ App.tsx
@@ -1,36 +1,31 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components'
-import AppLoading from 'expo-app-loading';
-import { 
- useFonts,
- Poppins_400Regular,
- Poppins_500Medium,
- Poppins_700Bold 
- } from '@expo-google-fonts/poppins'
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
-
-
-import theme from './src/global/styles/theme'
-import { Dashboard } from './src/Screens/Dashboard';
-import { Register } from './src/Screens/Register';
-
+import theme from "./src/global/styles/theme";
+import { Dashboard } from "./src/Screens/Dashboard";
+import { Register } from "./src/Screens/Register";
 
 export default function App() {
- const [fontsLoaded] = useFonts({
-   Poppins_400Regular,
-   Poppins_500Medium,
-   Poppins_700Bold
- });
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
 
- if(!fontsLoaded) {
-   return <AppLoading />
- }
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
 
-  return(
-    <ThemeProvider theme={theme} > 
-      <Dashboard />
+  return (
+    <ThemeProvider theme={theme}>
+      <Register />
     </ThemeProvider>
-    
-  ) 
+  );
 }
-
--- src/Screens/Register/index.tsx
+++ src/Screens/Register/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 
+import { Container, Header, Title, Form, Fields } from "./styles";
 
+import { Input } from "../../components/Form/Input";
+import { Button } from "../../components/Form/Button";
 
 export function Register() {
-    return(
-        <>
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+      <Form>
+        <Fields>
+          <Input placeholder="Nome" />
+          <Input placeholder="Preço" />
+        </Fields>
 
-        </>
-    )
+        <Button title="Enviar" />
+      </Form>
+    </Container>
+  );
 }
